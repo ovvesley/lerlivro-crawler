eccad41b2bce9e79d96d821044ad08cd0c0a5eba
Wesley Ferreira <wsf.ley@gmail.com>
2019-07-07 00:47:25 -0300

    funcional

--- src/interface.py
+++ src/interface.py
@@ -1,7 +1,7 @@
 from ler_livro_crawler import Crawler
 ##quantidade de pagina
-
-
 ler_livro = Crawler()
-ler_livro.set_pages(2,3)
+ler_livro.set_pages(3,5)
 ler_livro.start()
+print(len(ler_livro.array_book))
+
--- src/ler_livro_crawler.py
+++ src/ler_livro_crawler.py
@@ -1,16 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 import json
-
+import urllib
 class Crawler():
     __link_list = []
     array_book = []
     def __init__(self):
-        print('Bem vindo ao Crawler LER LIVRO')
-        
+        print('Bem vindo ao Crawler LER LIVRO')        
     def set_pages(self, start=2, end=622):
         self.__link_list = [('http://lelivros.love/page/'+ str(indexI) +'/') for indexI in range(start, end)]
-
     def start(self):        
        for link in self.__link_list:
             value = self.__get_books_from_page(link)
@@ -18,7 +16,8 @@ class Crawler():
                 book = json.loads(book_json)
                 book['Download'] = self.__get_book(book['Link'])
                 #book_js = json.dump(book)
-                self.array_book.append(book)               
+                #self.__download_book(book['Titulo'],book['Download'][2])
+                self.array_book.append(book)             
 
     def __get_books_from_page(self, url):
         page = requests.get(url)
@@ -47,6 +46,9 @@ class Crawler():
                 if(indexI.img['alt'] != 'Ler Online'):
                     links_book_download.append(indexI['href'])
         return links_book_download
-
-    def download_book():
-        pass
+    def __download_book(self,title, url):
+        opener=urllib.request.build_opener()
+        opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+        urllib.request.install_opener(opener)
+        urllib.request.urlretrieve(url,title+'.mobi')
+    

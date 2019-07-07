c05c52bb84646b2873faec5624a41a057a0280ac
Wesley Ferreira <wsf.ley@gmail.com>
2019-07-06 22:52:09 -0300

    getBook and Class LerLivroCrawler

--- src/main.py
+++ src/main.py
@@ -1,17 +1,24 @@
 from bs4 import BeautifulSoup
 import requests
-url = "http://lelivros.love/page/2"
 
-def get_books_from_page(url):
-    page = requests.get(url)
-    page_soup = BeautifulSoup(page.content,"html.parser")
-    uls = page_soup.find('ul', {'class': 'products'})
-    links_book = []
-    for indexI in uls:
-        if (indexI.name == 'li'):
-            print(indexI.a)
-            links_book.append(indexI.a['href'])
-    print(links_book)
-
-get_books_from_page(url)
-
+class LerLivroCrawler():
+    def get_books_from_page(self, url):
+        page = requests.get(url)
+        page_soup = BeautifulSoup(page.content,"html.parser")
+        uls = page_soup.find('ul', {'class': 'products'})
+        links_book = []
+        for indexI in uls:
+            if (indexI.name == 'li'):
+                print(indexI.a)
+                links_book.append(indexI.a['href'])
+        print(links_book)
+    def get_book(self, url):
+        page = requests.get(url)
+        page_soup = BeautifulSoup(page.content,"html.parser")
+        div = page_soup.find('div', {'class':'links-download'})
+        links_book_download = []
+        for indexI in div:
+            if(indexI.name == 'a'):
+                if("indexI.img['alt'] != 'Ler Online'"):
+                    links_book_download.append(indexI['href'])
+                print(links_book_download)

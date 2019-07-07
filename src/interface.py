af854980ee23200280dab03fa882984f750c5df7
Wesley Ferreira <wsf.ley@gmail.com>
2019-07-07 00:05:31 -0300

    array json de livros

---
+++ src/interface.py
@@ -0,0 +1,7 @@
+from ler_livro_crawler import Crawler
+##quantidade de pagina
+
+
+ler_livro = Crawler()
+ler_livro.set_pages(2,3)
+ler_livro.start()
--- src/ler_livro_crawler.py
+++ src/ler_livro_crawler.py
@@ -3,9 +3,24 @@ import requests
 import json
 
 class Crawler():
-    def __init__(self, url):
-        pass
-    def get_books_from_page(url, self):
+    __link_list = []
+    array_book = []
+    def __init__(self):
+        print('Bem vindo ao Crawler LER LIVRO')
+        
+    def set_pages(self, start=2, end=622):
+        self.__link_list = [('http://lelivros.love/page/'+ str(indexI) +'/') for indexI in range(start, end)]
+
+    def start(self):        
+       for link in self.__link_list:
+            value = self.__get_books_from_page(link)
+            for book_json in value:
+                book = json.loads(book_json)
+                book['Download'] = self.__get_book(book['Link'])
+                #book_js = json.dump(book)
+                self.array_book.append(book)               
+
+    def __get_books_from_page(self, url):
         page = requests.get(url)
         page_soup = BeautifulSoup(page.content,"html.parser")
         uls = page_soup.find('ul', {'class': 'products'})
@@ -21,15 +36,17 @@ class Crawler():
                     }
                 y = json.dumps(x)
                 links_book.append(y)
-        print(links_book)
         return links_book        
-    def get_book(url, self):
+    def __get_book(self, url):
         page = requests.get(url)
         page_soup = BeautifulSoup(page.content,"html.parser")
         div = page_soup.find('div', {'class':'links-download'})
         links_book_download = []
         for indexI in div:
             if(indexI.name == 'a'):
-                if("indexI.img['alt'] != 'Ler Online'"):
+                if(indexI.img['alt'] != 'Ler Online'):
                     links_book_download.append(indexI['href'])
-                print(links_book_download)
+        return links_book_download
+
+    def download_book():
+        pass
